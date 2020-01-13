5f4bb60e58e77561a7411e403da8e7d9723ed26f
roberttwomey <robert.twomey@gmail.com>
2020-01-13 11:58:00 -0800

    trying new bash script

--- player/play_loop.sh
+++ player/play_loop.sh
@@ -1,5 +1,6 @@
-streamer='streamlink -p /"omxplayer --timeout 30/" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
-until $streamer; do
+OMXCMD='omxplayer --timeout 30'
+STREAMER='streamlink -p ${OMXCMD} --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
+until $STREAMER; do
     echo "Streamer crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
