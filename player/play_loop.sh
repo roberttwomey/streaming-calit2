0004018502edf6a1bd667d2a911426136ccb74d1
roberttwomey <robert.twomey@gmail.com>
2020-01-13 11:51:16 -0800

    streamer updated

--- player/play_loop.sh
+++ player/play_loop.sh
@@ -1,5 +1,5 @@
-alias stream='streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
-until stream; do
+alias streamer='streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
+until streamer; do
     echo "Streamer crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
