7a4ac9da3454323299723159b87ea9637ccb4f38
roberttwomey <robert.twomey@gmail.com>
2020-01-13 11:53:33 -0800

    added variable

--- player/play_loop.sh
+++ player/play_loop.sh
@@ -1,5 +1,5 @@
-alias streamer='streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
-until streamer; do
+streamer = 'streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
+until $streamer; do
     echo "Streamer crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
