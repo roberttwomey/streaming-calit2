d8d9c57597ccb95bfdc53c8df4a7b726fa680ef3
roberttwomey <robert.twomey@gmail.com>
2020-01-13 12:05:28 -0800

    created service

--- player/launch_streaming.sh
+++ player/launch_streaming.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 10
-cd /home/pi/housemachine/streaming/
-/usr/bin/screen -dmS streaming streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p
+cd /home/pi/housemachine/streaming-calit2/player
+/usr/bin/screen -dmS streaming play_loop.sh
 
--- player/play_loop.sh
+++ player/play_loop.sh
@@ -1,5 +1,3 @@
-#OMXCMD='omxplayer --timeout 30'
-#STREAMER="streamlink -p \"omxplayer --timeout 30\" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p"
 until streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p; do
     echo "Streamer crashed with exit code $?.  Respawning.." >&2
     sleep 1
