9ed3a798556db0031391af8e3d6f7b10616d2db3
roberttwomey <robert.twomey@gmail.com>
2020-01-13 11:54:34 -0800

    removed extra space

--- player/play_loop.sh
+++ player/play_loop.sh
@@ -1,4 +1,4 @@
-streamer = 'streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
+streamer='streamlink -p "omxplayer --timeout 30" --player-fifo https://www.youtube.com/watch?v=fwfOrCyP8dM 720p'
 until $streamer; do
     echo "Streamer crashed with exit code $?.  Respawning.." >&2
     sleep 1
