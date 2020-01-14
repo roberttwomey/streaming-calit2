15cbc7b6d956112ea3f687d29e14f13abe8ad336
roberttwomey <robert.twomey@gmail.com>
2020-01-13 22:36:09 -0800

    added more streams including media files

--- mac_playback/mac_player.sh
+++ mac_playback/mac_player.sh
@@ -1,10 +1,18 @@
-mpv --fullscreen --length=10 \
+mpv --fullscreen --length=3 \
 --\{ \
 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
 https://www.youtube.com/watch?v=fwfOrCyP8dM \
 https://www.youtube.com/watch?v=qBQvaKlCXgE \
+https://www.youtube.com/watch?v=UvPdmXhvO1k \
 --\} \
---\{ --resume-playback --length=10 \
-https://www.youtube.com/watch?v=6hZMv7Ea0jk \
+--\{ --resume-playback \
 https://vimeo.com/337394969 \
-https://vimeo.com/369952685 --\} --loop-playlist
+https://vimeo.com/369952685 \
+https://www.youtube.com/watch?v=6hZMv7Ea0jk \
+https://youtu.be/2K6jEPX3D3U \
+media/SOMEONE\ documentation.mp4 \
+media/LobbyCapture_01_AE.mp4 \
+media/resonance\ -\ Ingrid\ Burrington.mp4 \
+media/optimization_method\ -\ Zachary\ Kaiser.mp4 \
+--\} \
+--loop-playlist
