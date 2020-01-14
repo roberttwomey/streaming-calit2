a4227d9eb2220f16813ae73bea62456ea66dbb39
MACmini Gallery <av_gallery@MACminis-Mac-mini.local>
2020-01-13 23:01:20 -0800

    added script for old mac

---
+++ mac_playback/mac_player_oldmac.sh
@@ -0,0 +1,20 @@
+mpv='/Applications/mpv.app/Contents/MacOS/mpv'
+
+$mpv --fullscreen --length=3 \
+--\{ \
+https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
+https://www.youtube.com/watch?v=fwfOrCyP8dM \
+https://www.youtube.com/watch?v=qBQvaKlCXgE \
+https://www.youtube.com/watch?v=UvPdmXhvO1k \
+--\} \
+--\{ --resume-playback \
+https://vimeo.com/337394969 \
+https://vimeo.com/369952685 \
+https://www.youtube.com/watch?v=6hZMv7Ea0jk \
+https://youtu.be/2K6jEPX3D3U \
+media/SOMEONE\ documentation.mp4 \
+media/LobbyCapture_01_AE.mp4 \
+media/resonance\ -\ Ingrid\ Burrington.mp4 \
+media/optimization_method\ -\ Zachary\ Kaiser.mp4 \
+--\} \
+--loop-playlist
