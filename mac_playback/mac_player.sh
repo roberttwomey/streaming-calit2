1cfc63a4c790530cf9dc6142692a5ff2edcdf915
Gallery Calit2 <gallerycalit2@Gallerys-Mac-mini.local>
2020-01-13 21:50:20 -0800

    added mac playback scripts

---
+++ mac_playback/mac_player.sh
@@ -0,0 +1,4 @@
+mpv --fullscreen --length=3 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
+--\{ --resume-playback --length=10 https://www.youtube.com/watch?v=6hZMv7Ea0jk \
+https://vimeo.com/337394969 \
+https://vimeo.com/369952685 --\} --loop-playlist
