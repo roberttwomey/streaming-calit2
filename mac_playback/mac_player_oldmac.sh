1a4d4d27a944bab558f97a5fc69ebbbcefe843f5
Robert Twomey <robert.twomey@gmail.com>
2020-01-14 10:34:19 -0800

    Update mac_player_oldmac.sh

--- mac_playback/mac_player_oldmac.sh
+++ mac_playback/mac_player_oldmac.sh
@@ -1,20 +1,20 @@
 mpv='/Applications/mpv.app/Contents/MacOS/mpv'
 
-$mpv --fullscreen --length=3 \
+$mpv --fullscreen --length=20 \
 --\{ \
-https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
 https://www.youtube.com/watch?v=fwfOrCyP8dM \
 https://www.youtube.com/watch?v=qBQvaKlCXgE \
-https://www.youtube.com/watch?v=UvPdmXhvO1k \
+https://www.youtube.com/watch?v=6AkYKzCr_uA \
+https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
+https://www.youtube.com/watch?v=3U78tCXwmLk \
+--\} \
+--\{ \
+--image-display-duration=20 http://hpwren.ucsd.edu/cameras/L/ms-n-mobo-c.jpg
 --\} \
 --\{ --resume-playback \
-https://vimeo.com/337394969 \
-https://vimeo.com/369952685 \
-https://www.youtube.com/watch?v=6hZMv7Ea0jk \
-https://youtu.be/2K6jEPX3D3U \
-media/SOMEONE\ documentation.mp4 \
-media/LobbyCapture_01_AE.mp4 \
-media/resonance\ -\ Ingrid\ Burrington.mp4 \
-media/optimization_method\ -\ Zachary\ Kaiser.mp4 \
+media/someone.mp4 \
+media/islands.mp4 \
+media/screensaver.mp4 \
+media/rubycam.mp4 \
 --\} \
 --loop-playlist
