3aee52725da49842dcc3fb275154432750cd29f0
Robert Twomey <robert.twomey@gmail.com>
2020-01-14 10:48:50 -0800

    Update mac_player_oldmac.sh

--- mac_playback/mac_player_oldmac.sh
+++ mac_playback/mac_player_oldmac.sh
@@ -7,9 +7,10 @@ https://www.youtube.com/watch?v=qBQvaKlCXgE \
 https://www.youtube.com/watch?v=6AkYKzCr_uA \
 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
 https://www.youtube.com/watch?v=3U78tCXwmLk \
+http://gallerycam.ucsd.edu/mjpg/video.mjpg \
 --\} \
 --\{ \
---image-display-duration=20 http://hpwren.ucsd.edu/cameras/L/ms-n-mobo-c.jpg
+--image-display-duration=20 http://hpwren.ucsd.edu/cameras/L/ms-n-mobo-c.jpg \
 --\} \
 --\{ --resume-playback \
 media/someone.mp4 \
@@ -17,4 +18,4 @@ media/islands.mp4 \
 media/screensaver.mp4 \
 media/rubycam.mp4 \
 --\} \
---loop-playlist
+--loop-playlist --shuffle
