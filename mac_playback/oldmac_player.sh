4bcbc1bf00601a600dad9b249c38d0d2e0a6b169
Robert Twomey <robert.twomey@gmail.com>
2020-01-14 11:39:36 -0800

    Update oldmac_player.sh

--- mac_playback/oldmac_player.sh
+++ mac_playback/oldmac_player.sh
@@ -10,7 +10,7 @@ https://www.youtube.com/watch?v=3U78tCXwmLk \
 http://gallerycam.ucsd.edu/mjpg/video.mjpg \
 --\} \
 --\{ \
---image-display-duration=40 http://hpwren.ucsd.edu/cameras/L/ms-n-mobo-c.jpg \
+--image-display-duration=40 http://hpwren.ucsd.edu/cameras/L/wc-w-mobo-c.jpg \
 --\} \
 --\{ --resume-playback \
 media/someone.mp4 \
