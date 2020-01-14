2b130be17ffdb8bee106fed1057f7ea4b3f3353d
Robert Twomey <robert.twomey@gmail.com>
2020-01-14 12:21:31 -0800

    Update mac_player.sh

--- mac_playback/mac_player.sh
+++ mac_playback/mac_player.sh
@@ -1,11 +1,13 @@
 mpv --fullscreen --length=40 \
 --\{ \
 https://www.youtube.com/watch?v=fwfOrCyP8dM \
-https://www.youtube.com/watch?v=qBQvaKlCXgE \
 https://www.youtube.com/watch?v=6AkYKzCr_uA \
 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
 https://www.youtube.com/watch?v=3U78tCXwmLk \
 http://gallerycam.ucsd.edu/mjpg/video.mjpg \
+--\} \
+--\{ \
+--start=+20 https://www.youtube.com/watch?v=qBQvaKlCXgE \
 --\} \
 --\{ \
 --image-display-duration=40 http://hpwren.ucsd.edu/cameras/L/wc-w-mobo-c.jpg \
