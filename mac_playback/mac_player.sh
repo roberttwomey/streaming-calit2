9b33a7914cdb39b3979749385ec09d3668db5f08
Gallery Calit2 <gallerycalit2@Gallerys-Mac-mini.local>
2020-01-13 21:57:40 -0800

    proper streaming script

--- mac_playback/mac_player.sh
+++ mac_playback/mac_player.sh
@@ -1,4 +1,10 @@
-mpv --fullscreen --length=3 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
---\{ --resume-playback --length=10 https://www.youtube.com/watch?v=6hZMv7Ea0jk \
+mpv --fullscreen --length=10 \
+--\{ \
+https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
+https://www.youtube.com/watch?v=fwfOrCyP8dM \
+https://www.youtube.com/watch?v=qBQvaKlCXgE \
+--\} \
+--\{ --resume-playback --length=10 \
+https://www.youtube.com/watch?v=6hZMv7Ea0jk \
 https://vimeo.com/337394969 \
 https://vimeo.com/369952685 --\} --loop-playlist
