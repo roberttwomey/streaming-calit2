0d40edfb7724e5da355ec647c2e72a3405a3a756
Robert Twomey <robert.twomey@gmail.com>
2020-01-14 13:45:04 -0800

    Update oldmac_player.sh

--- mac_playback/oldmac_player.sh
+++ mac_playback/oldmac_player.sh
@@ -2,8 +2,8 @@ mpv='/Applications/mpv.app/Contents/MacOS/mpv'
 
 $mpv --fullscreen --length=40 \
 --\{ \
-https://www.youtube.com/watch?v=fwfOrCyP8dM \
 https://www.youtube.com/watch?v=qBQvaKlCXgE \
+https://www.youtube.com/watch?v=fwfOrCyP8dM \
 https://www.youtube.com/watch?v=6AkYKzCr_uA \
 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
 https://www.youtube.com/watch?v=3U78tCXwmLk \
@@ -17,5 +17,6 @@ media/someone.mp4 \
 media/islands.mp4 \
 media/screensaver.mp4 \
 media/rubycam.mp4 \
+media/sycamore.mov \
 --\} \
 --loop-playlist --shuffle
