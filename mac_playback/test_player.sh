3d34a6494267fd903e913ab9fbf8bdee6203d299
roberttwomey <robert.twomey@gmail.com>
2020-01-15 10:30:12 -0800

    added test_player

---
+++ mac_playback/test_player.sh
@@ -0,0 +1,22 @@
+mpv --fullscreen --length=40 \
+youtube.com/channel/UCVszUW2gHcfPEAt7H5dg_CA/live
+
+# --\{ \
+# https://www.youtube.com/watch?v=qBQvaKlCXgE \
+# https://www.youtube.com/watch?v=fwfOrCyP8dM \
+# https://www.youtube.com/watch?v=6AkYKzCr_uA \
+# https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
+# https://www.youtube.com/watch?v=3U78tCXwmLk \
+# http://gallerycam.ucsd.edu/mjpg/video.mjpg \
+# --\} \
+# --\{ \
+# --image-display-duration=40 http://hpwren.ucsd.edu/cameras/L/wc-w-mobo-c.jpg \
+# --\} \
+# --\{ --resume-playback \
+# media/someone.mp4 \
+# media/islands.mp4 \
+# media/screensaver.mp4 \
+# media/rubycam.mp4 \
+# media/sycamore.mov \
+# --\} \
+# --loop-playlist --shuffle
