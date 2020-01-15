8a2766b3b3f4d8f123be523371c2f19052b358d9
Robert Twomey <robert.twomey@gmail.com>
2020-01-15 10:53:50 -0800

    Update oldmac_player.sh

--- mac_playback/oldmac_player.sh
+++ mac_playback/oldmac_player.sh
@@ -2,11 +2,12 @@ mpv='/Applications/mpv.app/Contents/MacOS/mpv'
 
 $mpv --fullscreen --length=40 \
 --\{ \
-https://www.youtube.com/watch?v=qBQvaKlCXgE \
-https://www.youtube.com/watch?v=fwfOrCyP8dM \
+https://www.youtube.com/channel/UCVszUW2gHcfPEAt7H5dg_CA/live \ # eunsu
+https://www.youtube.com/watch?v=qBQvaKlCXgE \ # amyalexander
+https://www.youtube.com/watch?v=fwfOrCyP8dM \ # fishcam
 https://www.youtube.com/watch?v=ccE03cUg29o \ # calum
 https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
-https://www.youtube.com/watch?v=3U78tCXwmLk \
+https://www.youtube.com/watch?v=3U78tCXwmLk \ # onedollarhotel
 http://gallerycam.ucsd.edu/mjpg/video.mjpg \
 --\} \
 --\{ \
