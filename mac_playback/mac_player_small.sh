1bb8dcfc91cc9c763bfd752a958bcb29633c311b
roberttwomey <robert.twomey@gmail.com>
2020-02-06 10:58:01 -0800

    added small and full playlists for mac and old mac

---
+++ mac_playback/mac_player_small.sh
@@ -0,0 +1,25 @@
+mpv='/usr/local/bin/mpv'
+# mpv --fullscreen --length=40 \
+
+$mpv --fullscreen --length=40 \
+--\{ \
+https://www.youtube.com/watch?v=I3LhqBK153Q \
+https://www.youtube.com/watch?v=qBQvaKlCXgE \
+https://www.youtube.com/watch?v=fwfOrCyP8dM \
+https://www.youtube.com/watch?v=ccE03cUg29o \
+https://cams.cdn-surfline.com/cdn-wc/wc-lajollashores/playlist.m3u8 \
+https://www.youtube.com/watch?v=3U78tCXwmLk \
+http://gallerycam.ucsd.edu/mjpg/video.mjpg \
+--\} \
+--\{ \
+--image-display-duration=40 http://hpwren.ucsd.edu/cameras/L/wc-w-mobo-c.jpg \
+--\} \
+--\{ --resume-playback \
+media/someone.mp4 \
+media/islands.mp4 \
+media/screensaver.mp4 \
+media/rubycam.mp4 \
+media/sycamore.mpg \
+--\} \
+--loop-playlist --shuffle
+
